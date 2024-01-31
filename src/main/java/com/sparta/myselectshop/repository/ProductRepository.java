ff5136f193d5b3884c16c6197f3c26fb4e3fd232
kudongku <kudongku@naver.com>
2024-01-31 15:41:15 +0900

    chore: repository 에 query문 주석 남김

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -1,6 +1,5 @@
 package com.sparta.myselectshop.repository;
 
-import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.entity.User;
 import org.springframework.data.domain.Page;
@@ -11,4 +10,12 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByUser(User user, Pageable pageable);
 
     Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
+    /*
+    select *
+    from product p
+        left join productFolder pf
+            on p.id = pf.product_id
+    where p.user_id = ?;
+        and pf.folder_id = ?;
+     */
 }
